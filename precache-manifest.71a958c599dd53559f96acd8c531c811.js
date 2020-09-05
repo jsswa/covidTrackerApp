460a746b8e068da19fbd0f2acf49c7b3eb002e8f
Vayango <vayango@hotmail.fr>
2020-09-05 22:39:02 +0200

    Updates

---
+++ precache-manifest.71a958c599dd53559f96acd8c531c811.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b99d6759b7f18fc0632bef9a7f221547",
+    "url": "/covidTrackerApp/index.html"
+  },
+  {
+    "revision": "4c31da7a3c22a0cf8ace",
+    "url": "/covidTrackerApp/static/css/main.79e70f6c.chunk.css"
+  },
+  {
+    "revision": "22410160da97a196004c",
+    "url": "/covidTrackerApp/static/js/2.464a8a4e.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/covidTrackerApp/static/js/2.464a8a4e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4c31da7a3c22a0cf8ace",
+    "url": "/covidTrackerApp/static/js/main.de440931.chunk.js"
+  },
+  {
+    "revision": "ea78957eb3f1ac9f1654",
+    "url": "/covidTrackerApp/static/js/runtime-main.fdbbd7e6.js"
+  },
+  {
+    "revision": "c46a5c144e131656a0e8d2026bd4ff09",
+    "url": "/covidTrackerApp/static/media/image2.c46a5c14.jpg"
+  }
+]);
